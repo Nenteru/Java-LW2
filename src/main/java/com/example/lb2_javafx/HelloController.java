3919be22db3d14750dbeda0a67aec7717dac382f
Nenteru <it.requiem@gmail.com>
2024-11-14 19:16:27 +0300

    Добавлена возможность выбрать фигуру по номеру. Добавлена возможность рисовать фигуру по ПКМ.

--- src/main/java/com/example/lb2_javafx/HelloController.java
+++ src/main/java/com/example/lb2_javafx/HelloController.java
@@ -1,61 +1,74 @@
 package com.example.lb2_javafx;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
+import javafx.scene.input.MouseEvent;
+import model.*;
+import model.Shape;
 
-import java.util.Objects;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
-
+public class HelloController implements Initializable  {
     @FXML
     private Canvas canvas;
-
     @FXML
-    private Button draw;
-
-
+    private ColorPicker color;
     @FXML
-    private TextField textField;
-
-
-    @FXML
-    private ColorPicker colorPicker;
-
-    @FXML
-    protected void onHelloButtonClick() {
-
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        Color color = colorPicker.getValue();
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-        String command = textField.getText();
-        if (Objects.equals(command, "Линия")) {
-            Line line = new Line(10, 100, color);
-            line.draw(gc);
+    private TextField textF;
+    ShapeFactory shapeFactory = new ShapeFactory();
+    Shape shape = null;
+    GraphicsContext gr;
+    private boolean isDragging = false;
+    private double dragOffsetX = 0;
+    private double dragOffsetY = 0;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        gr = canvas.getGraphicsContext2D();
+    }
+    public void onMouseClick(MouseEvent mouseEvent) {
+        double x = mouseEvent.getX();
+        double y = mouseEvent.getY();
+        gr.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        shape = shapeFactory.createShape(Integer.parseInt(textF.getText()), x, y);
+        shape.setColor(color.getValue());
+        shape.draw(gr);
+        dragOffsetX = mouseEvent.getX() - shape.getX();
+        dragOffsetY = mouseEvent.getY() - shape.getY();
+    }
+    public void OnClick() {
+        if(shape != null){
+            shape.setColor(color.getValue());
+            shape.draw(gr);
         }
-        if(Objects.equals(command, "Квадрат")) {
-            Rectangle rectangle = new Rectangle(100,200,color);
-            rectangle.draw(gc);
+    }
+    public void MouseDragged(MouseEvent mouseEvent) {
+        if (shape != null) {
+            isDragging = true;
+            double newX = mouseEvent.getX() - dragOffsetX;
+            double newY = mouseEvent.getY() - dragOffsetY;
+            shape.setPosition(newX, newY);
+            gr.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            shape.draw(gr);
         }
-        if(Objects.equals(command, "Треугольник")) {
-            Triangle triangle = new Triangle(color);
-            triangle.draw(gc);
+    }
+    public void MouseReleased(MouseEvent mouseEvent) {
+        if (isDragging) {
+            isDragging = false;
+            double newX = mouseEvent.getX() - dragOffsetX;
+            double newY = mouseEvent.getY() - dragOffsetY;
+            shape.setPosition(newX, newY);
+            gr.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            shape.draw(gr);
         }
-        if(Objects.equals(command, "Круг")) {
-            Circle circle = new Circle(color);
-            circle.draw(gc);
-        }
-        else
-            System.out.println("Error!");
     }
 
 }
